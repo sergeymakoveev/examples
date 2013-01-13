270faf2e018c7d615de9722ba14072ffabab55a3
Sergey Makoveev <sergey@makoveev.info>
2013-01-13 14:50:20 -0500

    [add] logger.js

---
+++ 2013.01.trends-of-frontend/logger.js
@@ -0,0 +1,27 @@
+function Logger(){
+    
+    var This = this;
+    
+    this.log = function(text, color){
+        var d = document.createElement('div');
+        d.innerHTML = text;
+        if( color )
+            d.style.color = color;
+        This.el.appendChild(d);
+    };
+    this.clear = function(){
+        This.el.innerHTML = '';
+    }
+    
+    this.button = document.createElement('input');
+    this.button.setAttribute('type', 'button');
+    this.button.setAttribute('value', 'Clear log');
+    this.button.onclick = this.clear;
+    document.body.appendChild(this.button);
+    
+    this.el = document.createElement('div');
+    this.el.style.border='1px dotted #000';
+    this.el.style.backgroundColor='#ccc';
+    this.el.style.minHeight='200px';
+    document.body.appendChild(this.el);
+};
